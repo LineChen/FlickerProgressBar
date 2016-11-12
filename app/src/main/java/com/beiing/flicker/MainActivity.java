c96888d421d71e55982848b56a5877fbcc24840c
LineChen <15764230067@163.com>
2016-11-12 11:06:00 +0800

    add round support & fix reset function

--- app/src/main/java/com/beiing/flicker/MainActivity.java
+++ app/src/main/java/com/beiing/flicker/MainActivity.java
@@ -4,22 +4,27 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-import com.beiing.flikerprogressbar.FlikerBar;
 import com.beiing.flikerprogressbar.FlikerProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener , Runnable{
 
-    FlikerBar flikerProgressBar;
+    FlikerProgressBar flikerProgressBar;
+    FlikerProgressBar roundProgressbar;
+
+    Thread downLoadThread;
 
     Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             flikerProgressBar.setProgress(msg.arg1);
+            roundProgressbar.setProgress(msg.arg1);
             if(msg.arg1 == 100){
                 flikerProgressBar.finishLoad();
+                roundProgressbar.finishLoad();
             }
         }
     };
@@ -28,50 +33,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        flikerProgressBar = (FlikerBar) findViewById(R.id.flikerbar);
+        flikerProgressBar = (FlikerProgressBar) findViewById(R.id.flikerbar);
+        roundProgressbar = (FlikerProgressBar) findViewById(R.id.round_flikerbar);
 
-        flikerProgressBar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!flikerProgressBar.isFinish()){
-                    flikerProgressBar.toggle();
-                }
-            }
-        });
+        flikerProgressBar.setOnClickListener(this);
+        roundProgressbar.setOnClickListener(this);
 
         downLoad();
     }
 
 
     public void reLoad(View view) {
-        if(flikerProgressBar.isFinish()){
+
+        downLoadThread.interrupt();
             // 重新加载
-            flikerProgressBar.reset();
-            downLoad();
-        }
+        flikerProgressBar.reset();
+        roundProgressbar.reset();
+
+        downLoad();
     }
 
     private void downLoad() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(true){
-                    try {
-                        float progress = flikerProgressBar.getProgress();
-                        progress  += 2;
-                        Thread.sleep(200);
-                        Message message = handler.obtainMessage();
-                        message.arg1 = (int) progress;
-                        handler.sendMessage(message);
-                        if(progress == 100){
-                            break;
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        downLoadThread = new Thread(this);
+        downLoadThread.start();
     }
 
+    @Override
+    public void onClick(View v) {
+        if(!flikerProgressBar.isFinish()){
+            flikerProgressBar.toggle();
+            roundProgressbar.toggle();
+
+            if(flikerProgressBar.isStop()){
+                downLoadThread.interrupt();
+            } else {
+                downLoad();
+            }
+
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while( ! downLoadThread.isInterrupted()){
+                float progress = flikerProgressBar.getProgress();
+                progress  += 2;
+                Thread.sleep(200);
+                Message message = handler.obtainMessage();
+                message.arg1 = (int) progress;
+                handler.sendMessage(message);
+                if(progress == 100){
+                    break;
+                }
+            }
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- flickerprogressbar/src/main/java/com/beiing/flikerprogressbar/FlikerProgressBar.java
+++ flickerprogressbar/src/main/java/com/beiing/flikerprogressbar/FlikerProgressBar.java
@@ -4,37 +4,44 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
- * Created by chenliu on 2016/8/26.<br/
- * 描述：
+ * Created by chenliu on 2016/8/26.<br/>
+ * 描述：添加圆角支持 on 2016/11/11
  * </br>
  */
 public class FlikerProgressBar extends View implements Runnable{
-
     private PorterDuffXfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP);
 
     private int DEFAULT_HEIGHT_DP = 35;
 
-    private int BORDER_WIDTH_DP = 1;
+    private int borderWidth;
 
-    private float MAX_PROGRESS = 100f;
+    private float maxProgress = 100f;
 
     private Paint textPaint;
 
     private Paint bgPaint;
 
+    private Paint pgPaint;
+
     private String progressText;
 
-    private Rect textBouds;
+    private Rect textRect;
+
+    private RectF bgRectf;
 
     /**
      * 左右来回移动的滑块
@@ -79,7 +86,11 @@ public class FlikerProgressBar extends View implements Runnable{
 
     private int textSize;
 
+    private int radius;
+
     private Thread thread;
+
+    BitmapShader bitmapShader;
 
     public FlikerProgressBar(Context context) {
         this(context, null, 0);
@@ -91,25 +102,35 @@ public class FlikerProgressBar extends View implements Runnable{
 
     public FlikerProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
         initAttrs(attrs);
     }
 
     private void initAttrs(AttributeSet attrs) {
-        if (attrs != null) {
-            TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.FlikerProgressBar);
-            textSize = (int) ta.getDimension(R.styleable.FlikerProgressBar_textSize, dp2px(12));
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.FlikerProgressBar);
+        try {
+            textSize = (int) ta.getDimension(R.styleable.FlikerProgressBar_textSize, 12);
             loadingColor = ta.getColor(R.styleable.FlikerProgressBar_loadingColor, Color.parseColor("#40c4ff"));
             stopColor = ta.getColor(R.styleable.FlikerProgressBar_stopColor, Color.parseColor("#ff9800"));
+            radius = (int) ta.getDimension(R.styleable.FlikerProgressBar_radius, 0);
+            borderWidth = (int) ta.getDimension(R.styleable.FlikerProgressBar_borderWidth, 1);
+        } finally {
             ta.recycle();
         }
     }
 
     private void init() {
-        bgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        bgPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
+        bgPaint.setStyle(Paint.Style.STROKE);
+        bgPaint.setStrokeWidth(borderWidth);
+
+        pgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pgPaint.setStyle(Paint.Style.FILL);
+
         textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         textPaint.setTextSize(textSize);
-        textBouds = new Rect();
+
+        textRect = new Rect();
+        bgRectf = new RectF(borderWidth, borderWidth, getMeasuredWidth() - borderWidth, getMeasuredHeight() - borderWidth);
 
         if(isStop){
             progressColor = stopColor;
@@ -124,7 +145,7 @@ public class FlikerProgressBar extends View implements Runnable{
     }
 
     private void initPgBimap() {
-        pgBitmap = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        pgBitmap = Bitmap.createBitmap(getMeasuredWidth() - borderWidth, getMeasuredHeight() - borderWidth, Bitmap.Config.ARGB_8888);
         pgCanvas = new Canvas(pgBitmap);
         thread = new Thread(this);
         thread.start();
@@ -140,7 +161,7 @@ public class FlikerProgressBar extends View implements Runnable{
         int height = 0;
         switch (heightSpecMode){
             case MeasureSpec.AT_MOST:
-                height = (int) dp2px(DEFAULT_HEIGHT_DP);
+                height = dp2px(DEFAULT_HEIGHT_DP);
                 break;
             case MeasureSpec.EXACTLY:
             case MeasureSpec.UNSPECIFIED:
@@ -159,18 +180,16 @@ public class FlikerProgressBar extends View implements Runnable{
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//背景
+        //背景
         drawBackGround(canvas);
 
-//进度
-        drawProgress();
+        //进度
+        drawProgress(canvas);
 
-        canvas.drawBitmap(pgBitmap, 0, 0, null);
-
-//进度text
+         //进度text
         drawProgressText(canvas);
 
-//变色处理
+        //变色处理
         drawColorProgressText(canvas);
     }
 
@@ -179,31 +198,33 @@ public class FlikerProgressBar extends View implements Runnable{
      * @param canvas
      */
     private void drawBackGround(Canvas canvas) {
-        bgPaint.setStyle(Paint.Style.STROKE);
         bgPaint.setColor(progressColor);
-        bgPaint.setStrokeWidth(dp2px(BORDER_WIDTH_DP));
-        canvas.drawRect(0, 0, getWidth(), getHeight(), bgPaint);
+        //left、top、right、bottom不要贴着控件边，否则border只有一半绘制在控件内,导致圆角处线条显粗
+        canvas.drawRoundRect(bgRectf, radius, radius, bgPaint);
     }
 
     /**
      * 进度
      */
-    private void drawProgress() {
-        bgPaint.setStyle(Paint.Style.FILL);
-        bgPaint.setStrokeWidth(0);
-        bgPaint.setColor(progressColor);
+    private void drawProgress(Canvas canvas) {
+        pgPaint.setColor(progressColor);
 
-        float right = (progress / MAX_PROGRESS) * getMeasuredWidth();
+        float right = (progress / maxProgress) * getMeasuredWidth();
         pgCanvas.save(Canvas.CLIP_SAVE_FLAG);
         pgCanvas.clipRect(0, 0, right, getMeasuredHeight());
         pgCanvas.drawColor(progressColor);
         pgCanvas.restore();
 
         if(!isStop){
-            bgPaint.setXfermode(xfermode);
-            pgCanvas.drawBitmap(flikerBitmap, flickerLeft, 0, bgPaint);
-            bgPaint.setXfermode(null);
+            pgPaint.setXfermode(xfermode);
+            pgCanvas.drawBitmap(flikerBitmap, flickerLeft, 0, pgPaint);
+            pgPaint.setXfermode(null);
         }
+
+        //控制显示区域
+        bitmapShader = new BitmapShader(pgBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        pgPaint.setShader(bitmapShader);
+        canvas.drawRoundRect(bgRectf, radius, radius, pgPaint);
     }
 
     /**
@@ -213,9 +234,9 @@ public class FlikerProgressBar extends View implements Runnable{
     private void drawProgressText(Canvas canvas) {
         textPaint.setColor(progressColor);
         progressText = getProgressText();
-        textPaint.getTextBounds(progressText, 0, progressText.length(), textBouds);
-        int tWidth = textBouds.width();
-        int tHeight = textBouds.height();
+        textPaint.getTextBounds(progressText, 0, progressText.length(), textRect);
+        int tWidth = textRect.width();
+        int tHeight = textRect.height();
         float xCoordinate = (getMeasuredWidth() - tWidth) / 2;
         float yCoordinate = (getMeasuredHeight() + tHeight) / 2;
         canvas.drawText(progressText, xCoordinate, yCoordinate, textPaint);
@@ -227,11 +248,11 @@ public class FlikerProgressBar extends View implements Runnable{
      */
     private void drawColorProgressText(Canvas canvas) {
         textPaint.setColor(Color.WHITE);
-        int tWidth = textBouds.width();
-        int tHeight = textBouds.height();
+        int tWidth = textRect.width();
+        int tHeight = textRect.height();
         float xCoordinate = (getMeasuredWidth() - tWidth) / 2;
         float yCoordinate = (getMeasuredHeight() + tHeight) / 2;
-        float progressWidth = (progress / MAX_PROGRESS) * getMeasuredWidth();
+        float progressWidth = (progress / maxProgress) * getMeasuredWidth();
         if(progressWidth > xCoordinate){
             canvas.save(Canvas.CLIP_SAVE_FLAG);
             float right = Math.min(progressWidth, xCoordinate + tWidth * 1.1f);
@@ -243,19 +264,21 @@ public class FlikerProgressBar extends View implements Runnable{
 
     public void setProgress(float progress){
         if(!isStop){
-            this.progress = progress;
+            if(progress < maxProgress){
+                this.progress = progress;
+            } else {
+                this.progress = maxProgress;
+                finishLoad();
+            }
             invalidate();
         }
-    }
-
-    public float getProgress() {
-        return progress;
     }
 
     public void setStop(boolean stop) {
         isStop = stop;
         if(isStop){
             progressColor = stopColor;
+            thread.interrupt();
         } else {
             progressColor = loadingColor;
             thread = new Thread(this);
@@ -267,14 +290,6 @@ public class FlikerProgressBar extends View implements Runnable{
     public void finishLoad() {
         isFinish = true;
         setStop(true);
-    }
-
-    public boolean isStop() {
-        return isStop;
-    }
-
-    public boolean isFinish() {
-        return isFinish;
     }
 
     public void toggle(){
@@ -290,19 +305,45 @@ public class FlikerProgressBar extends View implements Runnable{
     @Override
     public void run() {
         int width = flikerBitmap.getWidth();
-        while (!isStop){
-            flickerLeft += dp2px(5);
-            float progressWidth = (progress / MAX_PROGRESS) * getMeasuredWidth();
-            if(flickerLeft >= progressWidth){
-                flickerLeft = -width;
-            }
-            postInvalidate();
-            try {
+        try {
+            while (!isStop && !thread.isInterrupted()){
+                flickerLeft += dp2px(5);
+                float progressWidth = (progress / maxProgress) * getMeasuredWidth();
+                if(flickerLeft >= progressWidth){
+                    flickerLeft = -width;
+                }
+                postInvalidate();
                 Thread.sleep(20);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
+        }catch (InterruptedException e) {
+            e.printStackTrace();
         }
+    }
+
+    /**
+     * 重置
+     */
+    public void reset(){
+        setStop(true);
+        progress = 0;
+        isFinish = false;
+        isStop = false;
+        progressColor = loadingColor;
+        progressText = "";
+        flickerLeft = -flikerBitmap.getWidth();
+        initPgBimap();
+    }
+
+    public float getProgress() {
+        return progress;
+    }
+
+    public boolean isStop() {
+        return isStop;
+    }
+
+    public boolean isFinish() {
+        return isFinish;
     }
 
     private String getProgressText() {
@@ -320,22 +361,8 @@ public class FlikerProgressBar extends View implements Runnable{
         return text;
     }
 
-    /**
-     * 重置
-     */
-    public void reset(){
-        progress = 0;
-        isFinish = false;
-        isStop = false;
-        progressColor = loadingColor;
-        progressText = "";
-
-        initPgBimap();
-    }
-
-    private float dp2px(int dp){
+    private int dp2px(int dp){
         float density = getContext().getResources().getDisplayMetrics().density;
-        return dp * density;
+        return (int) (dp * density);
     }
-
 }
